51f6e638d475df5b59bca7330537c65dda449430
Hernán Forigua Upegui <Herfox@HERFOX.local>
2016-10-11 21:20:17 -0500

    Server Sync: Module Creator, Custom Attributes, Aprove Custome

---
+++ SalesForce/testform.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Herfox
+ * Date: 10/11/16
+ * Time: 9:31 AM
+ */
+
+echo "HERFOX_BEGIN -- ";
+echo('<pre>');
+var_dump($_POST);
+echo('</pre>');
+echo " -- HERFOX END";
